0d3218f6a09edadf156191f911b35b6a45dd579f
Gauravpatidar87 <gauravpatidar5577@gmail.com>
2024-06-20 23:53:46 +0530

    my first react website

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import './index.css'
+
+import React from 'react'
+import Weather from './components/Weather';
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Weather/>
+    </div>
+  )
+}
+
+export default App
+
